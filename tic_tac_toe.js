c5ecc0b2adc1fd9777a41499d730e29bfc689092
Kristyn Bryan <kristynrb@gmail.com>
2015-06-09 14:34:21 -0400

    Update tic_tac_toe.js
    Included how to announce a tie.

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,7 +1,9 @@
 console.log("js sheet is linked properly");
 console.log("checking the push");
 
-var count = 0;
+var $counter = 0;
+
+$winner = false;
 
 var playerOne = {
 	pName: "",
@@ -84,7 +86,7 @@ var game = {
  				$tr = $td.parent(),
 			    $allTrs = $('tr'),
 				$trChildren = $tr.children();
-				// var count = count + 1;
+				$counter = $counter + 1;
 
 			var row = $allTrs.index($tr),
 			    col = $trChildren.index($td);
@@ -100,75 +102,64 @@ var game = {
 
 	checkForWinner: function (rowNumber, columnNumber) {
 		console.log(rowNumber, columnNumber);
-		// row:
-		// if (var count = 9) {
-		// 		var tie = "true"
-		// 		game.clearBoard();
-		// };
 
 		if ($board[rowNumber][0].text() === $board[rowNumber][1].text() && 
 			$board[rowNumber][1].text() === $board[rowNumber][2].text()) {
-			
-			game.currentPlayer().tie = false;
-			// var $tie = false; 
 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
+			$winner = true;
 		};
 
 		// column
 		if ($board[0][columnNumber].text() === $board[1][columnNumber].text() && 
 			$board[1][columnNumber].text() === $board[2][columnNumber].text()) {
 
-			var $tie = game.currentPlayer().tie;
-			var $tie = false; 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
+			$winner = true;
 		};
 
 		// diagonals 
 		if ($board[0][0].text() === "X" && $board[1][1].text() === "X" && 
 			$board[2][2].text() === "X") {
 			
-			var $tie = game.currentPlayer().tie;
-			var $tie = false; 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
+			$winner = true;
 		};
 
 		if ($board[0][0].text() === "O" && $board[1][1].text() === "O" && 
 			$board[2][2].text() === "O") {
-			
-			var $tie = game.currentPlayer().tie;
-			var $tie = false; 
 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
+			$winner = true;
 		};
 
 		if ($board[0][2].text() === "X" && $board[1][1].text() === "X" && 
 			$board[2][0].text() === "X") {
-			
-			var $tie = game.currentPlayer().tie;
-			var $tie = false; 
 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
+			$winner = true;
 		};
 
 		if ($board[0][2].text() === "O" && $board[1][1].text() === "O" && 
 			$board[2][0].text() === "O") {
-			
-			game.currentPlayer().tie = false;
-			// var $tie = false; 
+
 
 			game.currentPlayer().score = game.currentPlayer().score + 1;
 			game.clearBoard();
-		} 	
+			$winner = true;
+		}
+		if 	if ($counter === 9) {
+			game.clearBoard();
+		};
 	},
 
 		clearBoard: function() {
-			if ($tie = true) {
+			if ($winner != true) {
 				$( "#bandaidOnBulletWound").text("The game is a tie!");
 			} else {
 				$( "#bandaidOnBulletWound").text(game.currentPlayer().piece + " is the winner!");
